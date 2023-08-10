8c86afea7e75a2a126f33a15d0691222b7ee5621
akiko23 <usksoaoko@gmail.com>
2023-08-10 13:17:06 +0300

    nevermind

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ current_num = [0]
 
 @dp.message_handler(commands=['start'])
 async def test(msg: types.Message):
-    id = msg.from_user.id
+    uid = msg.from_user.id
 
-    if not db.user_exists(id):
+    if not db.user_exists(uid):
         await bot.send_message(id,
                                'Добро пожаловать в бота по выставке обьявлений! Для начала работы зайдите в меню '
                                'командой /menu. Для справки можете зайти в /help')
@@ -125,48 +125,6 @@ async def actions_with_advertisements(call: types.CallbackQuery):
 
         await bot.send_message(user_id, 'Выберите, что хотите изменить', reply_markup=markups.choose_param_to_change())
 
-        @dp.callback_query_handler(Text(startswith='change'), state=None)
-        async def start_changing_advertisement(change_call: types.CallbackQuery):
-            await bot.delete_message(change_call.from_user.id, change_call.message.message_id)
-            param_to_change.append(change_call.data.split('-')[1])
-
-            await bot.send_message(change_call.from_user.id, f'Отправьте новое {param_to_change[0]}',
-                                   reply_markup=markups.break_changing_process_keyboard)
-            await Advertisement.AdvertisementActions.change_param.set()
-
-            @dp.message_handler(content_types=['text', 'photo'], state=Advertisement.AdvertisementActions.change_param)
-            async def changing_process(msg: types.Message, state: FSMContext):
-                incorrect_format = 'Цена не может включать в себя буквы или быть больше 50000' if param_to_change[
-                                                                                                      0] == 'price' else 'Неправильный формат'
-                correct_format = 'Изменения успешно внесены✅'
-                try:
-                    value_to_change = int(msg.text) if param_to_change[0] == 'price' else msg.text
-
-                    if param_to_change[0] == 'photo':
-                        db.change_advertisement(unique_id=unique_id, param_to_change='photo_id',
-                                                value=msg.photo[0].file_id)
-                    elif param_to_change[0] == 'price':
-                        if value_to_change < 50000:
-                            db.change_advertisement(unique_id=unique_id, param_to_change=param_to_change[0],
-                                                    value=value_to_change)
-                        else:
-                            raise ValueError
-                    else:
-                        db.change_advertisement(unique_id=unique_id, param_to_change=param_to_change[0],
-                                                value=value_to_change)
-
-                    await bot.delete_message(msg.from_user.id, msg.message_id - 1)
-                    await bot.send_message(msg.from_user.id, correct_format, reply_markup=markups.back_to_main_menu)
-
-                    await state.finish()
-                    param_to_change.clear()
-                except Exception:
-                    await bot.delete_message(msg.from_user.id, msg.message_id - 1)
-                    await bot.send_message(msg.from_user.id, incorrect_format,
-                                           reply_markup=markups.back_to_main_menu)
-                    await state.finish()
-                param_to_change.clear()
-
     elif action == 'delete':
         await bot.delete_message(call.from_user.id, call.message.message_id)
         advert_json = call.as_json()
@@ -179,6 +137,48 @@ async def actions_with_advertisements(call: types.CallbackQuery):
         except Exception as e:
             await bot.send_message(user_id, "Объявление не удалено❌")
 
+
+@dp.callback_query_handler(Text(startswith='change'), state=None)
+async def start_changing_advertisement(change_call: types.CallbackQuery):
+    await bot.delete_message(change_call.from_user.id, change_call.message.message_id)
+    param_to_change.append(change_call.data.split('-')[1])
+
+    await bot.send_message(change_call.from_user.id, f'Отправьте новое {param_to_change[0]}',
+                            reply_markup=markups.break_changing_process_keyboard)
+    await Advertisement.AdvertisementActions.change_param.set()
+
+    @dp.message_handler(content_types=['text', 'photo'], state=Advertisement.AdvertisementActions.change_param)
+    async def changing_process(msg: types.Message, state: FSMContext):
+        incorrect_format = 'Цена не может включать в себя буквы или быть больше 50000' if param_to_change[
+                                                                                                0] == 'price' else 'Неправильный формат'
+        correct_format = 'Изменения успешно внесены✅'
+        try:
+            value_to_change = int(msg.text) if param_to_change[0] == 'price' else msg.text
+
+            if param_to_change[0] == 'photo':
+                db.change_advertisement(unique_id=unique_id, param_to_change='photo_id',
+                                        value=msg.photo[0].file_id)
+            elif param_to_change[0] == 'price':
+                if value_to_change < 50000:
+                    db.change_advertisement(unique_id=unique_id, param_to_change=param_to_change[0],
+                                                    value=value_to_change)
+                else:
+                    raise ValueError
+                else:
+                    db.change_advertisement(unique_id=unique_id, param_to_change=param_to_change[0],
+                                                value=value_to_change)
+
+                    await bot.delete_message(msg.from_user.id, msg.message_id - 1)
+                    await bot.send_message(msg.from_user.id, correct_format, reply_markup=markups.back_to_main_menu)
+
+                    await state.finish()
+                    param_to_change.clear()
+        except Exception:
+            await bot.delete_message(msg.from_user.id, msg.message_id - 1)
+            await bot.send_message(msg.from_user.id, incorrect_format,
+                                    reply_markup=markups.back_to_main_menu)
+            await state.finish()
+        param_to_change.clear()
 
 @dp.message_handler(content_types=['photo'], state=Advertisement.photo)
 async def set_advertisement_photo(message: types.Message):
@@ -193,28 +193,26 @@ async def set_advertisement_photo(message: types.Message):
 
 @dp.message_handler(content_types=['text'], state=Advertisement.name)
 async def set_advertisement_name(message: types.Message, state: FSMContext):
-    incorrect_statement = f'Запрещено использовать {", ".join([f"""{s}""" for s in forbidden_chars])}\nЗагрузка объявления не удалась❌',
+    incorrect_statement_msg = f'Запрещено использовать {", ".join([f"""{s}""" for s in forbidden_chars])}\nЗагрузка объявления не удалась❌',
 
     if check_valid_msg(message):
         await bot.send_message(message.from_user.id, "Теперь добавьте описание",
                                reply_markup=markups.break_load_process_keyboard)
-        db.set_something(db.get_last_id(message.from_user.id), 'name', message.text)
+        
 
         await Advertisement.description.set()
     else:
-        await bot.send_message(message.from_user.id, incorrect_statement)
-        db.delete_advertisement(message.from_user.id, db.get_last_id(message.from_user.id))
-
+        await bot.send_message(message.from_user.id, incorrect_statement_msg)
         await state.finish()
 
 
 @dp.message_handler(content_types=['text'], state=Advertisement.description)
-async def set_advertisement_description(message: types.Message):
+async def set_advertisement_description(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
-    db.set_something(db.get_last_id(user_id), 'description', message.text)
+    await state.update_data({"description": message.text})
 
     await bot.send_message(message.from_user.id, 'Введите цену(в рублях)',
-                           reply_markup=markups.break_load_process_keyboard)
+                           reply_markup=markups.interrupt_adv_creating)
     await Advertisement.price.set()
 
 
@@ -222,13 +220,10 @@ async def set_advertisement_description(message: types.Message):
 async def set_advertisement_price(message: types.Message, state: FSMContext):
     user_id = message.from_user.id
     try:
-        if int(message.text) <= 50000:
-            db.set_something(db.get_last_id(user_id), 'price', int(message.text))
-        else:
-            raise ValueError
-
+        if not int(message.text) <= 50000:
+            return bot.send_message(user_id, "Цена не может превышать 50000 рублей",)
+        
         last_user_advertisement = db.get_user_advertisements_data(user_id)[-1]
-
         await bot.send_photo(user_id, last_user_advertisement[3],
                              caption=f"Ваше объявление успешно сохранено✅\n"
                                      f"\nУникальный номер: {last_user_advertisement[0]}"
@@ -245,14 +240,10 @@ async def set_advertisement_price(message: types.Message, state: FSMContext):
 @dp.message_handler(content_types=['text'])
 async def get_text_from_user(msg: types.Message):
     try:
-        first_currency = msg.text.split(' ')[0].split('_')[1].upper()
-        quantity = int(msg.text.split(' ')[0].split('_')[0])
-        second_currency = msg.text.split(' ')[1].upper()
-
+        first_currency, quantity, second_currency = msg.text.upper().split()
         await bot.send_message(msg.from_user.id,
-                               f"{quantity} {first_currency} это {convert_currencies(first_currency, quantity, second_currency)} {second_currency}")
-
-    except Exception:
+                               f"{quantity} {first_currency} это {convert_currencies(first_currency, int(quantity), second_currency)} {second_currency}")
+    except:
         await bot.send_message(msg.from_user.id, 'Я не понимаю, что это значит')
 
 
