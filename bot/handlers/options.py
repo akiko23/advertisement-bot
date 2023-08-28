920fefab03d77547a9ae757888409ffe11735af4
akiko23 <usksoaoko@gmail.com>
2023-08-28 12:30:33 +0300

    split into multiple handlers actions_with_all_ads_menu

--- bot/handlers/options.py
+++ bot/handlers/options.py
@@ -8,7 +8,7 @@ from bot.functions.watch_ads import watch_others_ad, watch_user_ad
 from bot.states.ad_actions import CreateAdForm, SearchForAds, WatchAllAds, WatchUserAds
 import bot.markups.markups as mp
 
-from bot.markups.callback_data_models import MainAdMenuOption
+from bot.markups.callback_data_models import MainAdMenuOption, WatchAllAdsMenuOption
 
 
 """
@@ -57,33 +57,35 @@ async def watch_all_ads(call: types.CallbackQuery, state: FSMContext):
 
 
 # handlers on all ads watching
-@router.callback_query(WatchAllAds.choose_option, F.data.startswith("all_ads"))
+@router.callback_query(WatchAllAds.choose_option, WatchAllAdsMenuOption.filter(F.action == "watch_all"))
 async def choose_option(call: types.CallbackQuery, state: FSMContext, bot: Bot, db: Database):
-    logging.info(call.data.split(":")[1])
-    match call.data.split(":")[1]:
-        case "watch":
-            await bot.delete_message(call.from_user.id, call.message.message_id)
-            
-            all_ads = await db.get_all_others_ads(call.from_user.id)
-            logging.info(f"All_ads: {all_ads}")
-            if not all_ads:
-                return await call.message.answer("Пока что нет объявлений от других пользователей :(")
+        await bot.delete_message(call.from_user.id, call.message.message_id)
+        
+        all_ads = await db.get_all_others_ads(call.from_user.id)
+        if not all_ads:
+            return await call.message.answer("Пока что нет объявлений от других пользователей :(")
 
-            await state.set_state(WatchAllAds.on_watch)
-            await state.update_data({
-                "all_ads": all_ads,
-                "current_ad": 0
-            })
-            await watch_others_ad(
-                user_id=call.from_user.id,
-                msg_id=call.message.message_id,
-                state=state,
-                bot=bot,
-                db=db
-            )
-        case "search":
-            await state.set_state(SearchForAds.on_search)
-            await call.message.edit_text("Напишите, что бы вы хотели найти", reply_markup=mp.back_to_main_menu)
-        case "back_to_ad_menu":            
-            await state.clear()
-            await call.message.edit_text('Главное меню', reply_markup=mp.main_menu)
+        await state.set_state(WatchAllAds.on_watch)
+        await state.update_data({
+            "all_ads": all_ads,
+            "current_ad": 0
+        })
+        await watch_others_ad(
+            user_id=call.from_user.id,
+            msg_id=call.message.message_id,
+            state=state,
+            bot=bot,
+            db=db
+        )
+
+
+@router.callback_query(WatchAllAds.choose_option, WatchAllAdsMenuOption.filter(F.action == "search"))
+async def on_search(call: types.CallbackQuery, state: FSMContext):
+        await state.set_state(SearchForAds.on_search)
+        await call.message.edit_text("Напишите, что бы вы хотели найти", reply_markup=mp.back_to_main_menu)
+
+
+@router.callback_query(WatchAllAds.choose_option, WatchAllAdsMenuOption.filter(F.action == "back_to_ad_menu"))
+async def back_to_ad_menu(call: types.CallbackQuery, state: FSMContext):
+        await state.clear()
+        await call.message.edit_text('Главное меню', reply_markup=mp.main_menu)
